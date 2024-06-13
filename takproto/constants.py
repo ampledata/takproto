4d4cab564d9ff22da9a913f83241826059bcf38c
Brian Wallen <bwallen@gmail.com>
2024-06-13 13:48:49 -0400

    Remove %f in the ISO_8601_UTC datetime format string

--- takproto/constants.py
+++ takproto/constants.py
@@ -35,7 +35,7 @@ DEFAULT_PROTO_HEADER = bytearray(b"\xbf")
 DEFAULT_MESH_HEADER = bytearray(b"\xbf\x01\xbf")
 
 W3C_XML_DATETIME: str = "%Y-%m-%dT%H:%M:%S.%fZ"
-ISO_8601_UTC = "%Y-%m-%dT%H:%M:%S.%fZ"
+ISO_8601_UTC: str = "%Y-%m-%dT%H:%M:%SZ"
 
 
 class TAKProtoVer(Enum):
